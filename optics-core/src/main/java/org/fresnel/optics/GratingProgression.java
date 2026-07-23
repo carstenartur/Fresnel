b20ba9f1206b33081db4d72efca6178e7d436c52
Carsten Hammer <carsten.hammer@t-online.de>
2026-07-23 21:00:08 +0200

    Add variable-line grating progression laws

---
+++ optics-core/src/main/java/org/fresnel/optics/GratingProgression.java
@@ -0,0 +1,8 @@
+package org.fresnel.optics;
+
+/** Mathematical law used to vary local grating pitch over the active extent. */
+public enum GratingProgression {
+    LINEAR_PITCH,
+    LINEAR_SPATIAL_FREQUENCY,
+    LOGARITHMIC_PITCH
+}
