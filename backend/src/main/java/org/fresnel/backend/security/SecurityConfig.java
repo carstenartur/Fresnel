4133280bdb02659b5a0d963c15549b0b82a3c465
Carsten Hammer <carsten.hammer@t-online.de>
2026-08-23 20:39:27 +0200

    Use the Spring Security password encoder package

--- backend/src/main/java/org/fresnel/backend/security/SecurityConfig.java
+++ backend/src/main/java/org/fresnel/backend/security/SecurityConfig.java
@@ -7,8 +7,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.PasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
